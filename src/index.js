8bad4d5cab6179858bbec0a60382097733a8d008
shenchao <shenchao@shouqianba.cn>
2018-01-24 22:59:11 +0800

    redux基本使用

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
+// 1 新建store
+// 通过reducer建立
+// 根据老的state和action 生成新的state
+function counter(state = 0, action) {
+    switch(action.type){
+        case '加机关枪':
+            return state + 1
+        case '减机关枪':
+            return state - 1
+        default:
+            return 10
+    }
+}
+const store = createStore(counter)
+const init = store.getState()
+console.log(init)
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function listener() {
+    const current = store.getState()
+    console.log(`现在有机枪${current}把`)
+}
+
+store.subscribe(listener)
+
+//派发事件
+store.dispatch({type:'加机关枪'})
+console.log(store.getState())
